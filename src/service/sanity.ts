23df37374c7ca6cd79ecca25fa275b25eed78c75
yooss2006 <yooss2006@naver.com>
2023-05-10 19:08:04 +0900

    Feat: 이미지 URL로 변환

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
@@ -7,3 +9,8 @@ export const client = createClient({
   apiVersion: "2023-03-20",
   token: process.env.SANITY_SECRET_TOKEN, //데이터 수정이 있을 경우 토큰 명시
 });
+
+const builder = imageUrlBuilder(client);
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source).width(800).url();
+}
